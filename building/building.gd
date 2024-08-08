2ded6fe1650a31b3c37148d08581750fcbef3447
Tom <tom@rouillard.org>
2024-08-08 23:39:57 +0200

    update list

--- building/building.gd
+++ building/building.gd
@@ -2,3 +2,9 @@ extends Node2D
 
 class_name Building
 
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+			emit_signal("building_clicked", self)
+
+signal building_clicked
